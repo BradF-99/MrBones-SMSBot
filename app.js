1ebca9a9cc6f279f98677fa372e9608235d22833
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-11-05 22:33:51 +1000

    Added helmet to app core

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 
 const bonesRouter = require('./routes/bonesRouter');
@@ -12,7 +13,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use('/bones', bonesRouter);
 
