84bff3e760f78225ad1889facefb218c7567f5ae
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-11-05 23:26:25 +1000

    Updated bonesRouter to use new module

--- routes/bonesRouter.js
+++ routes/bonesRouter.js
@@ -1,33 +1,27 @@
 require('dotenv').config();
 
 const express = require('express');
-const axios = require('axios');
 const router = express.Router();
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const axios = require('axios');
+const twimlModule = require('../modules/twimlModule');
 
 router.post('/', async (req, res) => {
     try {
         res.setHeader('Content-Type', 'text/xml');
         res.statusCode = 200;
         const response = await getMrBones(req.body.Body);
-        const twiml = await textToTwiml(response);
+        const twiml = await twimlModule.textToTwiml(response);
         return res.end(twiml);
     } catch (e) {
         res.send(e);
     }
-  
 });
 
 async function getMrBones(query){
     const encodedQuery = encodeURI(query);
     const response = await axios.get(process.env.MRBONESAPIURL + encodedQuery);
     return response.data.reply;
-}
-
-async function textToTwiml(msg){
-    const twiml = new MessagingResponse();
-    twiml.message(msg);
-    return twiml.toString();
 }
 
 module.exports = router;
