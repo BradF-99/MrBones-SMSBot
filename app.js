8d57ce4607b79331c6bc2fb5e64f49eff5ab1fbd
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-11-06 18:25:44 +1000

    Added error handler to app core

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser');
 const bonesRouter = require('./routes/bonesRouter');
 const errorRouter = require('./routes/errorRouter');
 
+const twimlModule = require('./modules/twimlModule');
+
 const app = express();
 
 app.use(logger('dev'));
@@ -20,5 +22,12 @@ app.use(helmet());
 
 app.use('/bones', bonesRouter);
 app.use('/error', errorRouter);
+
+// error handler 
+app.use(async (err, req, res, next) => {
+    const twiml = await twimlModule.textToTwiml("Sorry, Mr Bones didn't respond. Try again later.");
+    return res.end(twiml);
+});
+  
 
 module.exports = app;
